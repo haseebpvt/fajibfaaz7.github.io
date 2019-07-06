0f7b7317e40aae17a1a0ad7fd175fa2c952753d3
Fajib <49675506+imfaazi@users.noreply.github.com>
2019-07-07 00:19:14 +0530

    css

--- javascript/sroll.js
+++ javascript/sroll.js
@@ -6,7 +6,7 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navanim").style.padding = "10px 20px 10px 20px";
     document.getElementById("navanim").style.backgroundColor =
-      "rgba(44, 62, 80,1)";
+      "rgba(17,17,17, 1)";
   } else {
     document.getElementById("navanim").style.padding = "40px 60px 20px 50px"; //top,right,bottom,left
     document.getElementById("navanim").style.backgroundColor = "rgba(0,0,0,0)";
